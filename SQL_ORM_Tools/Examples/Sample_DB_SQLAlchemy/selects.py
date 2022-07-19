adbcfa429d32d10a734aefe8afc5412d86d34e1e
Jordi Ricard Onrubia Palacios <jordirop@gmail.com>
2022-07-19 21:09:59 +0200

    Add files via upload

--- SQL_ORM_Tools/Examples/Sample_DB_SQLAlchemy/selects.py
+++ SQL_ORM_Tools/Examples/Sample_DB_SQLAlchemy/selects.py
@@ -1,4 +1,4 @@
-from sqlalchemy import create_engine, select, update, delete
+from sqlalchemy import create_engine, select
 from sqlalchemy.orm import Session
 from models import Employee, Customer, Call
 
@@ -24,15 +24,3 @@ with Session(engine) as session:
     calls = session.execute(select(Call).join(Customer, Call.customer_id==Customer.id).filter(Customer.city_id==1)).scalars().all()
     for call in calls:
         print(call.id)
-
-    print('------ UPDATE ------')
-    updated_id = session.execute(update(Employee).where(Employee.id == 1).values(first_name="Paprika").returning(Employee.id)).scalar_one_or_none()
-    session.commit()
-    if updated_id is not None:
-        employee = session.execute(select(Employee).filter_by(id=updated_id)).scalar()
-        print(employee.first_name)
-
-    print('------ DELETE ------')
-    deleted_id = session.execute(delete(Call).where(Call.id == 1).returning(Call.id)).scalar_one_or_none()
-    print(deleted_id)
-    session.commit()
