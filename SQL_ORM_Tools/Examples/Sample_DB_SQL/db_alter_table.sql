3a75b3e58937eb139646e9c0b464f06b56610d42
Jordi Ricard Onrubia Palacios <jordirop@gmail.com>
2024-12-28 21:34:54 +0100

    Delete SQL_ORM_Tools directory

--- SQL_ORM_Tools/Examples/Sample_DB_SQL/db_alter_table.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE "city" (
-    "id" serial primary key not null,
-    "name" varchar(255) not null
-);
-
-ALTER TABLE "customer"
-ADD CONSTRAINT "FK_CityCustomer"
-FOREIGN KEY ("city_id") REFERENCES "city"("id");
